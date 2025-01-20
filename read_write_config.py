1c3c24c2f54e1f5bb36edfc32361655941aa489e
SneakerCP3 <55521600+SneakerCP3@users.noreply.github.com>
2025-01-20 14:53:04 +0800

    Update read_write_config.py

--- read_write_config.py
+++ read_write_config.py
@@ -9,7 +9,7 @@ import os
 import configparser
 
 
-cur_path = r"D:\\"
+cur_path = r"C:\\"
 conf_path = os.path.join(cur_path, 'utool_conf.ini')
 config = configparser.ConfigParser()
 config.read(conf_path, encoding="utf-8")
@@ -19,11 +19,19 @@ def get_values(section):
     keys = config.options(section)
     values = [config.get(section, key).split("\n") for key in keys]
     pairs = dict(zip(keys, values))
-
     return pairs
+
+def get_setting_value(key):
+    value = config.get("settings", key)
+    return value
+
+def get_query_value(key):
+    value = config.get("query", key)
+    return value
 
 
 if __name__ == '__main__':
 
     print(get_values(section="local_file"))
+
 
